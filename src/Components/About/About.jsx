d4cae90b42b7252d0ef02ed765ae06051a5debe6
Vadim <litvinenkov7@gmail.com>
2026-03-22 18:31:34 +0200

    NEw

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,39 +3,53 @@ import { useInView } from "react-intersection-observer";
 import style from "./About.module.css";
 
 export default function About() {
-  const likes = ["Высокое качество", "Трендовые модели", "Удобное расположение"];
+  const likes = [
+    "Высокое качество",
+    "Трендовые модели",
+    "Удобное расположение",
+  ];
 
   return (
     <div className={style.container}>
       <div className={style.about_container}>
         <div className={style.about_left}>
-          <h2 className={style.title}>О нас</h2>
-          <hr className={style.hr} />
-          <p>
-            VIANN — твій простір сили та стилю. Жіночий фітнес-одяг, який
-            мотивує рухатись вперед, виглядати бездоганно та відчувати себе
-            впевнено кожного дня.
-          </p>
-          <hr className={style.hr} />
-          <div className={style.likes}>
-            {likes.map((text, i) => {
-              const { ref, inView } = useInView({
-                triggerOnce: false, // срабатывает только один раз
-                threshold: 0.3,    // 30% элемента должно быть видно
-              });
+          <div className={style.top}>
+            <h2 className={style.title}>О нас</h2>
+            <hr className={style.hr} />
+            <p>
+              VIANN — твій простір сили та стилю. Жіночий фітнес-одяг, який
+              мотивує рухатись вперед, виглядати бездоганно та відчувати себе
+              впевнено кожного дня.
+            </p>
+            <hr className={style.hr} />
+            <div className={style.likes}>
+              {likes.map((text, i) => {
+                const { ref, inView } = useInView({
+                  triggerOnce: false,
+                  threshold: 0.3,
+                });
 
-              return (
-                <div
-                  key={i}
-                  ref={ref}
-                  className={`${style.fcItem} ${inView ? style.showItem : ""}`}
-                >
-                  <FcLike /> <span>{text}</span>
-                </div>
-              );
-            })}
+                return (
+                  <div
+                    key={i}
+                    ref={ref}
+                    className={`${style.fcItem} ${inView ? style.showItem : ""}`}
+                  >
+                    <FcLike /> <span>{text}</span>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <button className={style.button}>Больше фото в Instagram</button>
+          <button className={style.button}>
+            <a
+              href="https://www.instagram.com/viann_ua/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Больше фото в Instagram
+            </a>
+          </button>
         </div>
         <div className={style.about_right}>
           <img src="/photo7.jpg" alt="VIANN" />
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,9 +11,24 @@ export default function Header() {
     <div className={styles.header_container}>
       <div ref={ref} className={styles.header_content}>
         <h1 className={inView ? styles.show : ""}>VIANN</h1>
-        <h2 className={inView ? styles.show : ""}>VIANN — магазин жіночого одягу для фітнесу</h2>
-        <p className={inView ? styles.show : ""}>твій простір сили та стилю</p>
-        <button className={inView ? styles.show : ""}>Смотреть каталог в Instagram</button>
+        <div className={styles.centre}>
+          <h2 className={inView ? styles.show : ""}>
+            VIANN — магазин жіночого одягу для фітнесу
+          </h2>
+          <p className={inView ? styles.show : ""}>
+            твій простір сили та стилю
+          </p>
+        </div>
+        <div className={styles.end}>
+          <p className={inView ? styles.show : ""}>
+            КОМФОРТ,СТИЛЬ,ЕФИКТИВНОСТЬ
+          </p>
+          <button className={inView ? styles.show : ""}>
+            <a href="https://www.instagram.com/viann_ua/" target="_blank">
+              Смотреть каталог в Instagram
+            </a>
+          </button>
+        </div>
       </div>
     </div>
   );
